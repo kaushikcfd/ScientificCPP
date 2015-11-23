ad3e59618e6b1e3558f0e26c6f3fad976bd52085
Kaushik <kaushikcfd@gmail.com>
2015-11-23 21:40:55 +0530

    Created the file Operations.cpp and starting to work on it.

---
+++ Essentials/Operations.cpp
@@ -0,0 +1,10 @@
+/**
+ * This file will contain the basic functions that define the operator overloading functions on vectors.
+ * Defined multiplication on it.
+ * **/
+
+#include <vector>
+
+using namespace std;
+
+
