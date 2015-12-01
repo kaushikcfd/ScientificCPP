83e5a42cf12a45f0f2e36f88443114446898de4b
Kaushik <kaushikcfd@gmail.com>
2015-12-01 05:39:22 +0530

    Minor changes

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 int main()
 {
 	vector<double> a,b;
-	stepFill(a,-15,0.1,15);
+	stepFill(a,-45,0.1,45);
 	for(int i = 0; i < a.size();i++)
 		b.push_back( a[i]*sin(a[i]));
 	plot(a,b,"NewPloT","xsin(x)");
